7cd3fabc40c2a422762a15637ac47f9caf67a756
Andrei Sergo <a.sergoucht@gmail.com>
2018-11-14 04:09:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,21 @@
 class Dog
-  def initializer(name, breed = "Mutt")
-    @
+  def initializer(name, breed = "Matt")
+    @name = name
+    @breed  = breed
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  
+  def breed
+    @breed
+  end
 end
